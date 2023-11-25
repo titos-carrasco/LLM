58975f20aa36f24589554ea93bccb480c6cddc87
Roberto Carrasco <titos.carrasco@gmail.com>
2023-11-25 12:38:04 -0300

    Ajustes estructura de directorios

--- Llama.cpp/Python/Chat.py
+++ Llama.cpp/Python/Chat.py
@@ -1,18 +1,19 @@
 from llama_cpp import Llama
 
 class Chat():
-    def __init__(self, model, system):
+    def __init__(self, model, user, assistant, system):
         self.model = model
+        self.user = user
+        self.assistant = assistant
         self.system = system
-        self.history = ''
+        self.history = system
         self.llm = Llama(model_path=model, n_ctx=4096, verbose=False)
+        #self.llm('', stream=False)
 
     def _generate(self, prompt):
-        # template = 'system context. USER: {prompt} ASSISTANT:'
-        prompt = f'{self.history}\nUSER: {prompt}\nASSISTANT:'
-
+        prompt = f'{self.history}\n{self.user} {prompt}\n{self.assistant}'
         response = ''
-        streamer= self.llm(self.system + prompt, stream=True, stop=['USER', 'ASSISTANT'], temperature=0.8, top_k=40, top_p=0.9, seed=-1)
+        streamer= self.llm(prompt, stream=True, stop=[self.user, self.assistant], max_tokens= 4096, temperature=0.8, top_k=40, top_p=0.9, seed=-1)
         for chunk in streamer:
             text = chunk['choices'][0]['text']
             print(text, end='', flush=True)
@@ -46,9 +47,14 @@ class Chat():
 
 
 #---
+path_models = "/mnt/sda5/roberto/LLM Models/gguf/"
+#model = 'TheBloke/vicuna-7b-v1.5.Q4_K_M.gguf'
+#model = 'TheBloke/llama-2-7b-chat.Q4_K_M.gguf'
+model = 'TheBloke/mistral-7b-openorca.Q4_K_M.gguf'
 system = """
 Debes responder todo en español de la manera más correcta posible.
 Eres un asistente muy cordial y respondes todo de manera resumida y con información verdadera.
 """
-chat = Chat('/home/roberto/Pruebas/llm/models/gguf/vicuna-7b-v1.5.Q4_K_M.gguf', system)
+
+chat = Chat(path_models + model, '|USER', '|ASSISTANT|', system)
 chat.run()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LLM
-Mis pruebas con modelos de lenguaje grandes (LLM)
+Mis pruebas con modelos de lenguaje grandes (LLM) utilizando sólo CPU
+
+* Llama.cpp: Inference of LLaMA model in pure C/C++ (https://github.com/ggerganov/llama.cpp)
